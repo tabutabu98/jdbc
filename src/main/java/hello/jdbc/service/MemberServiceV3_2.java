abfdc610eeb549e736e04b5b405ed3b5195b6ebb
syc <syc@partdb.com>
2023-02-06 15:28:01 +0900

    feat: 트랜잭션 문제 해결 - 트랜잭션 템플릿 @syc

---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -0,0 +1,65 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import java.sql.Connection;
+import java.sql.SQLException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+
+/**
+ * 트랜젝션 - 트랜젝션 매니저
+ */
+@Slf4j
+public class MemberServiceV3_2 {
+
+//    private final DataSource dataSource;
+//    private final PlatformTransactionManager transactionManager;
+    private final TransactionTemplate txTemplate;
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
+
+    public void accountTransfer(String formId, String toId, int money) throws SQLException {
+
+        txTemplate.executeWithoutResult(status -> {
+            try {
+                bizLogic(formId, toId, money);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private void bizLogic(String formId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(formId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(formId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+    private static void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true); // 트랜젝션 종료
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+}
